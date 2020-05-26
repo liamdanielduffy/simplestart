0c2ce6f75daf89ad9944191f520dc57043c07f3d
Liam Duffy <liamdanielduffy@gmail.com>
2020-05-25 22:33:32 -0400

    adds description to site meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <DefaultSeo
         title="simplestart"
+        description="A simple starting point for web applications"
       />
       <Component {...pageProps} />
     </>
