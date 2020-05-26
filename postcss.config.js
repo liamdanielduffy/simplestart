801f428ea471680ebd242b699cead334dc9c57e5
Liam Duffy <liamdanielduffy@gmail.com>
2020-05-25 21:57:22 -0400

    includes all js, jsx, ts, tsx files in purgecss rules

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,19 @@
 const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
-    content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+    content: [
+      "./pages/**/*.{js,jsx,ts,tsx}",
+      "./components/**/*.{js,jsx,ts,tsx}",
+    ],
     defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
   },
 ];
+
 module.exports = {
   plugins: [
     "postcss-import",
     "tailwindcss",
-    "autoprefixer",
     ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    "autoprefixer",
   ],
 };
