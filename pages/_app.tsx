ab78d102aa1ec09410ac3b72bd86d56634dede1f
Liam Duffy <liamdanielduffy@gmail.com>
2020-05-25 22:19:11 -0400

    gives the app a default title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import 'tailwind.css'
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo
+        title="simplestart"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
